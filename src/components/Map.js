98adcee8ac5e3b1611a2dcfc70858845d49bc0c5
Trent Segura <trnt@me.com>
2021-01-09 17:06:50 -0700

    please have lines

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react'
 import './Map.css';
 import mapbox from 'mapbox-gl';
+import { placeData } from '../data/data'
 
 export class Map extends Component {
 
     
     componentDidMount(){
+        const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
         const app = this.props.app
-        mapbox.accessToken = 'pk.eyJ1IjoibTEyLXRyZW50IiwiYSI6ImNrNDNuejljbjA0NzMzZW15eGk4OWMwdTEifQ.8rs6af8i7F8oeHDpbD_zQw';
+        mapbox.accessToken = 'pk.eyJ1IjoibTEyLXRyZW50IiwiYSI6ImNrZ2UxcHRzcjBqZmIycnFpdzh1MzlkYXAifQ.0UqT-ST0dFdnPhuVd54T5w';
        
-        var bounds = [
-            [-110.113384, 35.747004], // Southwest coordinates
-            [-102.113384, 39.747004] // Northeast coordinates
-        ]
+        // var bounds = [
+        //     [-110.113384, 35.747004], // Southwest coordinates
+        //     [-102.113384, 39.747004] // Northeast coordinates
+        // ]
 
         var map = new mapbox.Map({
             container: 'map',
             style:  "mapbox://styles/m12-trent/ckg9xlrw62gxc19lc9hbaylmf",
             center: [app.state.longitude, app.state.latitude],
             zoom: app.state.zoom,
-            minZoom: 8,
+            minZoom: 1,
             maxZoom: 13,
-            maxPitch: 60,
             attributionControl: false,
-            maxBounds: bounds
+            // maxBounds: bounds
             });
         
         map.scrollZoom.disable();
@@ -32,7 +33,19 @@ export class Map extends Component {
         const nav = new mapbox.NavigationControl();
         map.addControl(nav, 'bottom-right');
 
+        this.props.app.setState({
+            map: map
+        })
+
+        
+
+        
+
+
+
+
         map.on('load', function () {
+
             map.addSource('landlines', {
                 'type': 'geojson',
                 'data': {
@@ -40,9 +53,34 @@ export class Map extends Component {
                     'properties': {},
                     'geometry': {
                         'type': 'LineString',
-                        'coordinates': [                            
-                            [-106.3533684, 37.6788919],
-                            [-105.7009757, 37.9949705]
+                        'coordinates': [
+                            placeData[0].coordinates,
+                            placeData[1].coordinates,
+                            placeData[6].coordinates,
+                            placeData[11].coordinates,
+                            placeData[3].coordinates,
+                            placeData[16].coordinates,
+                            placeData[10].coordinates,
+                            placeData[6].coordinates,
+                            placeData[5].coordinates,
+                            placeData[12].coordinates,
+                            placeData[8].coordinates,
+                            placeData[4].coordinates,
+                            placeData[7].coordinates,
+                            placeData[9].coordinates,
+                            placeData[14].coordinates,
+                            placeData[4].coordinates,
+                            placeData[8].coordinates,
+                            placeData[11].coordinates,
+                            placeData[14].coordinates,
+                            placeData[15].coordinates,
+                            placeData[0].coordinates,
+                            placeData[2].coordinates,
+                            placeData[13].coordinates,
+                            placeData[4].coordinates,
+                            placeData[9].coordinates,
+                            placeData[2].coordinates,
+                            placeData[3].coordinates,
                             ]
                         }
                     }    
@@ -62,15 +100,12 @@ export class Map extends Component {
                     'line-width': 1.5
                  }
             });
+            
         })
-
-        this.props.app.setState({
-            map: map
-        });
     }
     
-    
     render() { 
+
 
         return (
             <div id="map">
